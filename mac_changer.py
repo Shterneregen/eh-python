009339977f69b74cdec0fa6f19893c669bee6d0d
shterneregen <shterneregen@gmail.com>
2020-04-30 23:50:06 +0300

    mac changer, check result

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import optparse
 import subprocess
+import re
 
 
 def get_arguments():
@@ -23,8 +24,26 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return str(mac_address_search_result.group(0))
+    else:
+        print("[-] Could not read MAC address")
+
+
 options = get_arguments()
+
+print(f'Current MAC = {str(get_current_mac(options.interface))}')
+
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print(f'[+] MAC address was successfully changed to {str(current_mac)}')
+else:
+    print(f'[-] MAC address did not get changed')
 
 # Second less secure option ()
 # subprocess.call(f'ifconfig {interface} down', shell=True)
