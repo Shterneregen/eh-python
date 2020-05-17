87dea5ab6d076754733bfc61ce85caf023d2e06b
shterneregen <shterneregen@gmail.com>
2020-05-18 02:29:04 +0300

    Resources scanner

--- crawler/scan_resources.py
+++ crawler/scan_resources.py
@@ -3,19 +3,17 @@ import argparse
 
 import requests
 
-
-# Example:
-# scan_subdomains.py -t google.com -sf file.txt
+# python scan_resources.py -t site.com -rf file.txt
 
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target", help="Target domain")
-    parser.add_argument("-sf", "--subdomains-file", dest="subdomains_file", help="File with list of subdomains")
+    parser.add_argument("-rf", "--resources-file", dest="resources_file", help="File with list of resources")
     options = parser.parse_args()
     if not options.target:
         parser.error("[-] Please specify a target, use --help for more info")
-    elif not options.subdomains_file:
-        parser.error("[-] Please specify a subdomains file, use --help for more info")
+    elif not options.resources_file:
+        parser.error("[-] Please specify a file with list of resources, use --help for more info")
     return options
 
 
@@ -26,15 +24,15 @@ def request(url):
         pass
 
 
-def scan_subdomains(target, subdomains_file):
+def scan_resources(target, subdomains_file):
     with open(subdomains_file, "r") as word_list_file:
         for line in word_list_file:
             word = line.strip()
-            test_url = word + "." + target
+            test_url = target + "/" + word
             response = request(test_url)
             if response:
-                print("[+] Discovered subdomain --> " + test_url)
+                print("[+] Discovered URL --> " + test_url)
 
 
 options = get_arguments()
-scan_subdomains(options.target, options.subdomains_file)
+scan_resources(options.target, options.resources_file)
